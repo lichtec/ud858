c83bb25009822c95a5d1d6a9e790f8d97591b818
Lichter <Christopher.Lichter@se2.com>
2016-08-22 13:03:33 -0500

    Lesson 4.6

--- Lesson_4/00_Conference_Central/models.py
+++ Lesson_4/00_Conference_Central/models.py
@@ -23,7 +23,16 @@ class Profile(ndb.Model):
     displayName = ndb.StringProperty()
     mainEmail = ndb.StringProperty()
     teeShirtSize = ndb.StringProperty(default='NOT_SPECIFIED')
+    conferenceKeysToAttend = ndb.StringProperty(repeated=True)
 
+# needed for conference registration
+class BooleanMessage(messages.Message):
+    """BooleanMessage-- outbound Boolean value message"""
+    data = messages.BooleanField(1)
+
+class ConflictException(endpoints.ServiceException):
+    """ConflictException -- exception mapped to HTTP 409 response"""
+    http_status = httplib.CONFLICT
 
 class ProfileMiniForm(messages.Message):
     """ProfileMiniForm -- update Profile form message"""
